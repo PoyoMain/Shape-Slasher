dddcfa94801fdbf0cb8d45f05fe28f32bc871fa5
PoyoMain <CLC9512@GMAIL.COM>
2025-02-01 17:26:18 -0500

    Map Generation 2.0

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,29 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Mathematics;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class MapGenerator : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private int numberOfRooms;
-    [SerializeField] private int numberOfNeighborsAllowed;
-    [SerializeField] private bool canReuseRooms;
+    //[SerializeField] private int numberOfNeighborsAllowed;
+    //[SerializeField] private bool canReuseRooms;
 
     [Header("References")]
     [SerializeField] private Room[] startRooms;
     [SerializeField] private Room[] bossRooms;
     [SerializeField] private List<Room> possibleRooms;
-    [SerializeField] private Room roomPrefab;
 
     private Room startRoom;
     private List<Room> rooms;
     private Room bossRoom;
 
-    private Room[,] roomGrid = new Room[11,11];
-    private Vector2Int currentGridPos = new(6, 6);
+    private Room[,] roomGrid = new Room[50,50];
+    private Vector2Int currentGridPos = new(25, 25);
+    private Room currentRoom;
 
     private Room RoomAtCurrentPos
     {
@@ -38,111 +37,18 @@ public class MapGenerator : MonoBehaviour
 
     private void Awake()
     {
-        //spawnedRooms = new();
-    }
+        rooms = new();
+        roomGrid = new Room[50, 50];
 
-    private void GenerateMap()
-    {
-        DespawnMap();
-
-        //List<Room> startingRooms = possibleRooms.Where(x => x.RoomType == RoomType.Starting).ToList();
-        //List<Room> bossRooms = possibleRooms.Where(x => x.RoomType == RoomType.Boss).ToList();
-        List<Room> standardRooms = possibleRooms.Where(x => x.RoomType == RoomType.Standard).ToList();
-
-        //Room roomToSpawn = startingRooms[UnityEngine.Random.Range(0, startingRooms.Count)];
-        //Room lastRoomSpawned = Instantiate(roomToSpawn, transform.position, Quaternion.identity);
-        //spawnedRooms.Add(lastRoomSpawned);
-
-        //for (int i = 1; i < standardRooms.Count - 1; i++)
-        //{
-        //    List<Room> correctRooms = standardRooms.Where(x => lastRoomSpawned.Compatible(x)).ToList();
-
-        //    if (correctRooms.Count == 0)
-        //    {
-        //        DespawnMap();
-        //        return false;
-        //    }
-
-        //    roomToSpawn = correctRooms[UnityEngine.Random.Range(0, correctRooms.Count)];
-        //    Vector2 directionToSpawnIn = lastRoomSpawned.DirectionToRoom(roomToSpawn);
-        //    lastRoomSpawned = Instantiate(roomToSpawn, (Vector2)transform.localPosition + (directionToSpawnIn * new Vector2(ROOM_BOUNDS_X, ROOM_BOUNDS_Y)), Quaternion.identity);
-        //    spawnedRooms.Add(lastRoomSpawned);
-        //}
-
-        //List<Room> potentialBossRooms = standardRooms.Where(x => lastRoomSpawned.Doors.Equals(x.Doors)).ToList();
-
-        //if (potentialBossRooms.Count == 0)
-        //{
-        //    DespawnMap();
-        //    return false;
-        //}
-
-        //roomToSpawn = potentialBossRooms[UnityEngine.Random.Range(0, potentialBossRooms.Count)];
-        //lastRoomSpawned = Instantiate(roomToSpawn, transform.position, Quaternion.identity);
-        //spawnedRooms.Add(lastRoomSpawned);
-
-        //return true;
-
-
-        Room lastRoomSpawned = startRoom = Instantiate(startRooms[UnityEngine.Random.Range(0, startRooms.Length)], transform.position, Quaternion.identity);
-        lastRoomSpawned.Init();
-        roomGrid[currentGridPos.x, currentGridPos.y] = lastRoomSpawned;
-
-        for (int i = 1; i < numberOfRooms; i++)
-        {
-            List<Room> compatibleRooms = new();
-            List<Door> validPrevDoors = new();
-
-            foreach (Door oldDoor in lastRoomSpawned.OpenDoors)
-            {
-                if (oldDoor.Direction == Direction.North && roomGrid[currentGridPos.x, currentGridPos.y + 1] == null) validPrevDoors.Add(oldDoor);
-                else if (oldDoor.Direction == Direction.East && roomGrid[currentGridPos.x + 1, currentGridPos.y] == null) validPrevDoors.Add(oldDoor);
-                else if (oldDoor.Direction == Direction.South && roomGrid[currentGridPos.x, currentGridPos.y - 1] == null) validPrevDoors.Add(oldDoor);
-                else if (oldDoor.Direction == Direction.West && roomGrid[currentGridPos.x - 1, currentGridPos.y] == null) validPrevDoors.Add(oldDoor);
-            }
-
-                // Find Compatible Rooms
-                foreach (Room stanRoom in standardRooms)
-            {
-                foreach (Door vDoor in validPrevDoors)
-                {
-                    foreach (Door newDoor in stanRoom.Doors)
-                    {
-                        if (vDoor.IsOpposite(newDoor, out Direction _))
-                        {
-                            compatibleRooms.Add(stanRoom);
-                        }
-                    }
-                }
-            }
-
-            //if (compatibleRooms.Count <= 0)
-            //{
-            //    DespawnMap();
-            //    GenerateMap();
-            //    return;
-            //}
-
-            Room roomToSpawn = compatibleRooms[UnityEngine.Random.Range(0, compatibleRooms.Count)];
-            Vector2Int dir = lastRoomSpawned.LinkRoom(roomToSpawn);
-            lastRoomSpawned.Init();
-            lastRoomSpawned = Instantiate(roomToSpawn, (Vector2)lastRoomSpawned.transform.position + (dir * new Vector2(ROOM_BOUNDS_X, ROOM_BOUNDS_Y)), Quaternion.identity);
-            lastRoomSpawned.Init();
-            roomGrid[currentGridPos.x + dir.x, currentGridPos.y + dir.y] = lastRoomSpawned;
-
-            compatibleRooms.Clear();
-            validPrevDoors.Clear();
-        }
     }
 
     [ContextMenu("Generate Map")]
     private void GenMap()
     {
-        if (mapGenCoroutine != null) StopCoroutine(nameof(MapGenCoroutine));
-        mapGenCoroutine = StartCoroutine(nameof(MapGenCoroutine));
+        if (mapGenCoroutine != null) StopCoroutine(nameof(MapCoroutine));
+        mapGenCoroutine = StartCoroutine(nameof(MapCoroutine));
         
     }
-
     private IEnumerator MapGenCoroutine()
     {
         DespawnMap();
@@ -153,7 +59,7 @@ public class MapGenerator : MonoBehaviour
         {
             DespawnMap();
             RoomAtCurrentPos = Instantiate(startRooms[UnityEngine.Random.Range(0, startRooms.Length)], transform.position, Quaternion.identity);
-            RoomAtCurrentPos.Init();
+            RoomAtCurrentPos.SetUpRoom(currentGridPos);
 
             for (int i = 1; i < numberOfRooms; i++)
             {
@@ -181,7 +87,7 @@ public class MapGenerator : MonoBehaviour
 
                 if (compatibleRooms.Count <= 0)
                 {
-                    currentGridPos.Set(6,6);
+                    currentGridPos.Set(25,25);
                     compatibleRooms.Clear();
                     validPrevDoors.Clear();
                     break;
@@ -190,12 +96,12 @@ public class MapGenerator : MonoBehaviour
                 Room roomToSpawn = compatibleRooms[UnityEngine.Random.Range(0, compatibleRooms.Count)];
 
                 Room spawnedRoom = Instantiate(roomToSpawn, (Vector2)RoomAtCurrentPos.transform.localPosition + new Vector2(ROOM_BOUNDS_X, ROOM_BOUNDS_Y), Quaternion.identity);
-                spawnedRoom.Init();
-
                 Vector2Int dir = RoomAtCurrentPos.LinkRoom(spawnedRoom);
+                
                 spawnedRoom.transform.localPosition = (Vector2)RoomAtCurrentPos.transform.localPosition + (dir * new Vector2(ROOM_BOUNDS_X, ROOM_BOUNDS_Y));
                 currentGridPos += dir;
                 RoomAtCurrentPos = spawnedRoom;
+                spawnedRoom.SetUpRoom(currentGridPos);
 
                 compatibleRooms.Clear();
                 validPrevDoors.Clear();
@@ -203,7 +109,7 @@ public class MapGenerator : MonoBehaviour
                 if (i == numberOfRooms - 1)
                 {
                     roomGenerated = true;
-                    currentGridPos.Set(6, 6);
+                    currentGridPos.Set(25, 25);
                 }
             }
 
@@ -211,12 +117,157 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
+    private IEnumerator MapCoroutine()
+    {
+        DespawnMap();
+        List<Room> standardRooms = possibleRooms.Where(x => x.RoomType == RoomType.Standard).ToList();
+
+        bool mapGenerated = false;
+        while (!mapGenerated)
+        {
+            DespawnMap();
+            List<Room> spawnedRooms = new();
+
+            currentRoom = Instantiate(startRooms[UnityEngine.Random.Range(0, startRooms.Length)], transform.position, Quaternion.identity, transform);
+            currentRoom.AssignPosition(new(0, 0));
+            spawnedRooms.Add(currentRoom);
+
+            for (int i = 1; i < numberOfRooms; i++)
+            {
+                bool compatibleRoomsFound = false;
+                List<Room> compatibleRooms = new();
+
+                while (!compatibleRoomsFound)
+                {
+                    foreach (Room stanRoom in standardRooms)
+                    {
+                        if (currentRoom.IsCompatibleWith(stanRoom, out Direction dir))
+                        {
+                            if (CheckIfRoomPlacingPositionIsEmpty(spawnedRooms, dir) && CheckIfPathsFromRoomAreEmpty(spawnedRooms, stanRoom, dir))
+                            {
+                                compatibleRooms.Add(stanRoom);
+                            }
+                        }
+                    }
+
+
+                    if (compatibleRooms.Count <= 0)
+                    {
+                        if (currentRoom.HasSpotsOpen)
+                        {
+                            Room replacementRoom = Instantiate(FindReplacementRoom(currentRoom), currentRoom.transform.position, Quaternion.identity, transform);
+                            replacementRoom.AssignPosition(currentRoom.RoomNumber);
+                            foreach (Room neighbor in currentRoom.Neighbors)
+                            {
+                                Direction dir = replacementRoom.DirectionToUnlinkedRoom(neighbor);
+                                replacementRoom.SetAdjacentRoom(neighbor, dir);
+                                neighbor.SetAdjacentRoom(replacementRoom, dir, true);
+                            }
+
+                            spawnedRooms.Remove(currentRoom);
+                            spawnedRooms.Add(replacementRoom);
+                            Destroy(currentRoom.gameObject);
+                            currentRoom = replacementRoom;
+                        }
+
+                        List<Room> roomsWithDoorsAvailable = spawnedRooms.Where(room => room.HasSpotsOpen).ToList();
+
+                        if (roomsWithDoorsAvailable.Count <= 0) break;
+
+                        currentRoom = roomsWithDoorsAvailable[^1];
+                    }
+                    else compatibleRoomsFound = true;
+                }
+
+                if (!compatibleRoomsFound) break;
+
+                // Spawn Room and add it to list
+                Room newRoom = Instantiate(compatibleRooms[UnityEngine.Random.Range(0, compatibleRooms.Count)], transform);
+                spawnedRooms.Add(newRoom);
+
+                // Determine direction and link rooms to each other
+                Direction direction = currentRoom.DirectionToUnlinkedRoom(newRoom);
+                currentRoom.SetAdjacentRoom(newRoom, direction);
+                newRoom.SetAdjacentRoom(currentRoom, direction, true);
+                
+                // Set new room position
+                newRoom.transform.localPosition = (Vector2)currentRoom.transform.localPosition + (DirectionToVectorDirection(direction) * new Vector2(ROOM_BOUNDS_X, ROOM_BOUNDS_Y));
+
+                // Set new room grid pos
+                newRoom.AssignPosition(currentRoom.RoomNumber + DirectionToVectorDirection(direction));
+
+                // Assign new current room
+                currentRoom = newRoom;
+
+                // If the right number of rooms has been spawned, end loop
+                if (i == numberOfRooms - 1)
+                {
+                    mapGenerated = true;
+                }
+
+                //Reset
+                compatibleRooms.Clear();
+            }
+
+            yield return null;
+        }
+    }
+
+    private Room FindReplacementRoom(Room currentRoom)
+    {
+
+        foreach (Room r in possibleRooms)
+        {
+            if (!(currentRoom.GetAdjacentRoom(Direction.North) != null == r.IsFacingDirection(Direction.North))) continue;
+            if (!(currentRoom.GetAdjacentRoom(Direction.East) != null == r.IsFacingDirection(Direction.East))) continue;
+            if (!(currentRoom.GetAdjacentRoom(Direction.South) != null == r.IsFacingDirection(Direction.South))) continue;
+            if (!(currentRoom.GetAdjacentRoom(Direction.West) != null == r.IsFacingDirection(Direction.West))) continue;
+
+            return r;
+        }
+
+        return null;
+    }
+
+    private bool CheckIfRoomPlacingPositionIsEmpty(List<Room> spawnedRooms, Direction dir)
+    {
+        foreach (Room room in spawnedRooms)
+        {
+            if (room.RoomNumber == currentRoom.RoomNumber + DirectionToVectorDirection(dir))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool CheckIfPathsFromRoomAreEmpty(List<Room> spawnedRooms, Room stanRoom, Direction dir)
+    {
+        foreach (Direction roomDirection in stanRoom.DirectionsFacing)
+        {
+            if (-DirectionToVectorDirection(roomDirection) != DirectionToVectorDirection(dir))
+            {
+                foreach (Room room in spawnedRooms)
+                {
+                    if (room.RoomNumber == (currentRoom.RoomNumber + DirectionToVectorDirection(dir)) + DirectionToVectorDirection(roomDirection))
+                    {
+
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
     [ContextMenu("Despawn Map")]
     private void DespawnMap()
     {
-        foreach (Room r in roomGrid)
+        foreach (Transform t in transform)
         {
-            if (r != null) Destroy(r.gameObject);
+            Destroy(t.gameObject);
         }
     }
 
@@ -232,6 +283,40 @@ public class MapGenerator : MonoBehaviour
 
         return Vector2.negativeInfinity;
     }
+
+    #region Utility Classes
+
+    private Direction VectorDirectionToDirection(Vector2Int directionVector)
+        => directionVector switch
+        {
+            var _ when directionVector == Vector2Int.up => Direction.North,
+            var _ when directionVector == Vector2Int.right => Direction.East,
+            var _ when directionVector == Vector2Int.down => Direction.South,
+            var _ when directionVector == Vector2Int.left => Direction.West,
+            _ => throw new System.NotImplementedException(),
+        };
+
+    private Direction VectorDirectionToDirection(Vector2 directionVector)
+        => directionVector switch
+        {
+            var _ when directionVector == Vector2Int.up => Direction.North,
+            var _ when directionVector == Vector2Int.right => Direction.East,
+            var _ when directionVector == Vector2Int.down => Direction.South,
+            var _ when directionVector == Vector2Int.left => Direction.West,
+            _ => throw new System.NotImplementedException(),
+        };
+
+    private Vector2Int DirectionToVectorDirection(Direction dir)
+        => dir switch
+        {
+            Direction.North => Vector2Int.up,
+            Direction.East => Vector2Int.right,
+            Direction.South => Vector2Int.down,
+            Direction.West => Vector2Int.left,
+            _ => throw new System.NotImplementedException(),
+        };
+
+    #endregion
 }
 
 public enum Direction
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Room : MonoBehaviour
@@ -14,19 +14,80 @@ public class Room : MonoBehaviour
     [Header("Doors")]
     [SerializeField] private List<Door> doors;
 
-
-
     [Header("Components")]
     [SerializeField] private CinemachineVirtualCamera cam;
 
     // Properties
     public RoomType RoomType => roomType;
     public List<Door> Doors => doors;
+    public List<Door> OpenDoors => openDoors;
+    public Vector2Int RoomNumber => roomNumber;
+
+    // Private Variables
+    private GameObject player;
 
     private List<Door> openDoors;
-    public List<Door> OpenDoors => openDoors;
+    private Vector2Int roomNumber;
+    private Room northNeighborRoom;
+    private Room eastNeighborRoom;
+    private Room southNeighborRoom;
+    private Room westNeighborRoom;
 
-    private GameObject player;
+    public List<Room> Neighbors
+    {
+        get
+        {
+            List<Room> n = new();
+            if (northNeighborRoom != null) n.Add(northNeighborRoom);
+            if (eastNeighborRoom != null) n.Add(eastNeighborRoom);
+            if (southNeighborRoom != null) n.Add(southNeighborRoom);
+            if (westNeighborRoom != null) n.Add(westNeighborRoom);
+            return n;
+        }
+    }
+
+    public bool HasSpotsOpen
+    {
+        get
+        {
+            if (DoorsOnNorthSide && northNeighborRoom == null) return true;
+            else if (DoorsOnEastSide && eastNeighborRoom == null) return true;
+            else if (DoorsOnSouthSide && southNeighborRoom == null) return true;
+            else if (DoorsOnWestSide && westNeighborRoom == null) return true;
+            return false;
+
+        }
+    }
+
+    private bool DoorsOnNorthSide
+    {
+        get => Doors.Any(door => door.Direction == Direction.North);
+    }
+    private bool DoorsOnEastSide
+    {
+        get => Doors.Any(door => door.Direction == Direction.East);
+    }
+    private bool DoorsOnSouthSide
+    {
+        get => Doors.Any(door => door.Direction == Direction.South);
+    }
+    private bool DoorsOnWestSide
+    {
+        get => Doors.Any(door => door.Direction == Direction.West);
+    }
+
+    public List<Direction> DirectionsFacing
+    {
+        get
+        {
+            List<Direction> dList = new();
+            if (northNeighborRoom == null && DoorsOnNorthSide) dList.Add(Direction.North); 
+            if (eastNeighborRoom == null && DoorsOnEastSide) dList.Add(Direction.East); 
+            if (southNeighborRoom == null && DoorsOnSouthSide) dList.Add(Direction.South); 
+            if (westNeighborRoom == null && DoorsOnWestSide) dList.Add(Direction.West);
+            return dList;
+        }
+    }
 
     private void Awake()
     {
@@ -39,14 +100,20 @@ public class Room : MonoBehaviour
         cam.Follow = player.transform;
     }
 
-    public void Init()
+    public void SetUpRoom(Vector2Int roomNum)
     {
         openDoors = doors.Where(x => !x.Linked).ToList();
+        roomNumber = roomNum;
     }
 
     public void LinkDoor(Door d)
     {
         OpenDoors.Remove(d);
+    }
+
+    public void AssignPosition(Vector2Int pos)
+    {
+        roomNumber = pos;
     }
 
     public Vector2Int LinkRoom(Room otherRoom)
@@ -74,6 +141,173 @@ public class Room : MonoBehaviour
         }
 
         return Vector2Int.zero;
+    }
+
+    public Door[] GetOpenDoors()
+    {
+        List<Door> dList = new();
+
+        if (northNeighborRoom == null)
+        {
+            dList.AddRange(Doors.Where(door => door.Direction == Direction.North).ToList());
+        }
+        if (eastNeighborRoom == null)
+        {
+            dList.AddRange(Doors.Where(door => door.Direction == Direction.East).ToList());
+        }
+        if (southNeighborRoom == null)
+        {
+            dList.AddRange(Doors.Where(door => door.Direction == Direction.South).ToList());
+        }
+        if (westNeighborRoom == null)
+        {
+            dList.AddRange(Doors.Where(door => door.Direction == Direction.West).ToList());
+        }
+
+        return dList.ToArray();
+    }
+
+    public void SetAdjacentRoom(Room adjacentRoom, Direction dir, bool opposite = false)
+    {
+        if (!opposite)
+        {
+            if (dir == Direction.North) northNeighborRoom = adjacentRoom;
+            else if (dir == Direction.East) eastNeighborRoom = adjacentRoom;
+            else if (dir == Direction.South) southNeighborRoom = adjacentRoom;
+            else if (dir == Direction.West) westNeighborRoom = adjacentRoom;
+        }
+        else
+        {
+            if (dir == Direction.North) southNeighborRoom = adjacentRoom;
+            else if (dir == Direction.East) westNeighborRoom = adjacentRoom;
+            else if (dir == Direction.South) northNeighborRoom = adjacentRoom;
+            else if (dir == Direction.West) eastNeighborRoom = adjacentRoom;
+        }
+        
+    }
+
+    public Room GetAdjacentRoom(Direction dir)
+        => dir switch
+        {
+            Direction.North => northNeighborRoom,
+            Direction.East => eastNeighborRoom,
+            Direction.South => southNeighborRoom,
+            Direction.West => westNeighborRoom,
+            _ => null
+        };
+
+    public bool IsCompatibleWith(Room otherRoom, out Direction direction)
+    {
+        if (northNeighborRoom == null && DoorsOnNorthSide)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.North1) == otherRoom.Doors.Any(door => door.Type == DoorType.South1))
+            && (Doors.Any(door => door.Type == DoorType.North2) == otherRoom.Doors.Any(door => door.Type == DoorType.South2))
+            && (Doors.Any(door => door.Type == DoorType.North3) == otherRoom.Doors.Any(door => door.Type == DoorType.South3)))
+            {
+                direction = Direction.North;
+                return true;
+            }
+        }
+        if (eastNeighborRoom == null && DoorsOnEastSide)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.East1) == otherRoom.Doors.Any(door => door.Type == DoorType.West1))
+            && (Doors.Any(door => door.Type == DoorType.East2) == otherRoom.Doors.Any(door => door.Type == DoorType.West2))
+            && (Doors.Any(door => door.Type == DoorType.East3) == otherRoom.Doors.Any(door => door.Type == DoorType.West3)))
+            {
+                direction = Direction.East;
+                return true;
+            }
+        }
+        if (southNeighborRoom == null && DoorsOnSouthSide)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.South1) == otherRoom.Doors.Any(door => door.Type == DoorType.North1))
+                        && (Doors.Any(door => door.Type == DoorType.South2) == otherRoom.Doors.Any(door => door.Type == DoorType.North2))
+                        && (Doors.Any(door => door.Type == DoorType.South3) == otherRoom.Doors.Any(door => door.Type == DoorType.North3)))
+            {
+                direction = Direction.South;
+                return true;
+            }
+        }
+        if (westNeighborRoom == null && DoorsOnWestSide)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.West1) == otherRoom.Doors.Any(door => door.Type == DoorType.East1))
+            && (Doors.Any(door => door.Type == DoorType.West2) == otherRoom.Doors.Any(door => door.Type == DoorType.East2))
+            && (Doors.Any(door => door.Type == DoorType.West3) == otherRoom.Doors.Any(door => door.Type == DoorType.East3)))
+            {
+                direction = Direction.West;
+                return true;
+            }
+        }
+
+
+        direction = Direction.North;
+        return false;
+    }
+
+    public Direction DirectionToUnlinkedRoom(Room otherRoom)
+    {
+        if (DoorsOnNorthSide && northNeighborRoom == null)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.North1) == otherRoom.Doors.Any(door => door.Type == DoorType.South1))
+            && (Doors.Any(door => door.Type == DoorType.North2) == otherRoom.Doors.Any(door => door.Type == DoorType.South2))
+            && (Doors.Any(door => door.Type == DoorType.North3) == otherRoom.Doors.Any(door => door.Type == DoorType.South3)))
+            {
+                return Direction.North;
+            }
+        }
+        
+        if (DoorsOnEastSide && eastNeighborRoom == null)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.East1) == otherRoom.Doors.Any(door => door.Type == DoorType.West1))
+            && (Doors.Any(door => door.Type == DoorType.East2) == otherRoom.Doors.Any(door => door.Type == DoorType.West2))
+            && (Doors.Any(door => door.Type == DoorType.East3) == otherRoom.Doors.Any(door => door.Type == DoorType.West3)))
+            {
+                return Direction.East;
+            }
+        }
+
+        if (DoorsOnSouthSide && southNeighborRoom == null)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.South1) == otherRoom.Doors.Any(door => door.Type == DoorType.North1))
+            && (Doors.Any(door => door.Type == DoorType.South2) == otherRoom.Doors.Any(door => door.Type == DoorType.North2))
+            && (Doors.Any(door => door.Type == DoorType.South3) == otherRoom.Doors.Any(door => door.Type == DoorType.North3)))
+            {
+                return Direction.South;
+            }
+        }
+
+        if (DoorsOnWestSide && westNeighborRoom == null)
+        {
+            if ((Doors.Any(door => door.Type == DoorType.West1) == otherRoom.Doors.Any(door => door.Type == DoorType.East1))
+            && (Doors.Any(door => door.Type == DoorType.West2) == otherRoom.Doors.Any(door => door.Type == DoorType.East2))
+            && (Doors.Any(door => door.Type == DoorType.West3) == otherRoom.Doors.Any(door => door.Type == DoorType.East3)))
+            {
+                return Direction.West;
+            }
+        }
+
+        return Direction.North;
+    }
+
+    public Direction DirectionToNeighbor(Room otherRoom)
+    {
+        Vector2Int dirVector = otherRoom.roomNumber - roomNumber;
+
+        return dirVector switch
+        {
+            var _ when dirVector == Vector2Int.up => Direction.North,
+            var _ when dirVector == Vector2Int.left => Direction.East,
+            var _ when dirVector == Vector2Int.down => Direction.South,
+            var _ when dirVector == Vector2Int.right => Direction.West,
+            _ => throw new System.NotImplementedException(),
+        };
+    }
+
+    public bool IsFacingDirection(Direction dir)
+    {
+        if (DirectionsFacing.Contains(dir)) return true;
+
+        return false;
     }
 }
 
@@ -148,3 +382,22 @@ public class Door
         else { directionGoing = Direction.North; return false; }
     }
 }
+
+//public class Side
+//{
+//    private Door door1;
+//    private Door door2;
+//    private Door door3;
+
+//    public Side(Door d1, Door d2, Door d3)
+//    {
+//        door1 = d1;
+//        door2 = d2;
+//        door3 = d3;
+//    }
+
+//    public bool Equals(Side otherSide)
+//    {
+//        if (otherSide.door1 != null)
+//    }
+//}
