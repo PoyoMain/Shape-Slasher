c83b65317b208c908829c375998908541e07e966
PoyoMain <CLC9512@GMAIL.COM>
2025-03-15 15:04:17 -0400

    Hitbox

--- Assets/Scripts/Player/PlayerHitbox.cs
+++ Assets/Scripts/Player/PlayerHitbox.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerHitbox : MonoBehaviour
 {
     [SerializeField] private Axis axis;
+
+    private const int ROTATION_FACINGRIGHT = 0;
+    private const int ROTATION_FACINGLEFT = 180;
     private enum Axis { Horizontal, Vertical } 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (axis == Axis.Horizontal)
         {
-            SendMessageUpwards("WallKnockback", collision.contacts[0].point);
+            if (transform.eulerAngles.y == ROTATION_FACINGLEFT) SendMessageUpwards("HitboxKnockback", Vector2.right);
+            else if (transform.eulerAngles.y == ROTATION_FACINGRIGHT) SendMessageUpwards("HitboxKnockback", Vector2.left);
         }
     }
 }
