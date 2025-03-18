deaaf9de633ce352fca772f29647225084416921
PoyoMain <CLC9512@GMAIL.COM>
2025-03-18 05:51:47 -0400

    Killing myself POSTPONED!! Credits scrolling works

--- Assets/Scripts/UI/CreditsMenu.cs
+++ Assets/Scripts/UI/CreditsMenu.cs
@@ -7,13 +7,23 @@ using UnityEngine.UI;
 
 public class CreditsMenu : MonoBehaviour
 {
+    [SerializeField] private RectTransform content;
+    [SerializeField] private float maxContentPos;
+
     private PlayerControls _playerControls;
     private PlayerControls.UIControlsActions controls;
 
     private Vector2 navigationInput;
     private bool navigationHit;
 
+    private Button[] buttons;
+    private Button currentButton;
+    private int currentButtonIndex = 1;
+
     private EventSystem eventSystem;
+
+    private float Step => maxContentPos / (buttons.Length - 1);
+
 
     private void OnEnable()
     { 
@@ -22,12 +32,14 @@ public class CreditsMenu : MonoBehaviour
         controls.Enable();
 
         eventSystem = EventSystem.current;
+
+        buttons = GetComponentsInChildren<Button>();
+        eventSystem.currentSelectedGameObject.TryGetComponent(out currentButton);
     }
 
     private void Update()
     {
         navigationInput = controls.Navigate.ReadValue<Vector2>();
-        navigationHit = controls.Navigate.WasPerformedThisFrame();
     }
 
     private void FixedUpdate()
@@ -37,18 +49,37 @@ public class CreditsMenu : MonoBehaviour
 
     private void HandleInput()
     {
-        if (navigationInput == Vector2.zero || !navigationHit) return;
+        if (navigationInput.y == 0) return;
 
-        print(eventSystem.currentSelectedGameObject.transform.position.y);
+        if (!eventSystem.currentSelectedGameObject.TryGetComponent(out Button eventCurrentButton)) return;
+
+        if (eventCurrentButton == currentButton) return;
+        
+        currentButton = eventCurrentButton;
+
+        Vector2 newPos = content.anchoredPosition;
+        if (navigationInput == Vector2.down)
+        {
+            if (currentButtonIndex == buttons.Length) return;
+
+            currentButtonIndex++;
+            newPos.y = Mathf.Clamp(newPos.y + Step, 0, maxContentPos);
+
+        }
+        else if (navigationInput == Vector2.up)
+        {
+            if (currentButtonIndex == 1) return;
+
+            currentButtonIndex--;
+            newPos.y = Mathf.Clamp(newPos.y - Step, 0, maxContentPos);
+        }
+
+        content.anchoredPosition = newPos;
+
     }
 
     private void OnDisable()
     {
         controls.Disable();
-    }
-
-    private void Print()
-    {
-
     }
 }
