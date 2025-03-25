aa42b211501b6ba1557965f574bc210a8134b1ab
PoyoMain <CLC9512@GMAIL.COM>
2025-03-25 06:33:35 -0400

    Adding in a hit VFX cause I hate sleep apparently

--- Assets/Scripts/Player/PlayerHitbox.cs
+++ Assets/Scripts/Player/PlayerHitbox.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class PlayerHitbox : MonoBehaviour
 {
     [SerializeField] private Axis axis;
+    [SerializeField] private ParticleSystem hitEffect;
 
     private const int ROTATION_FACINGRIGHT = 0;
     private const int ROTATION_FACINGLEFT = 180;
-    private enum Axis { Horizontal, Vertical } 
+    private const float COOLDOWN_TIME = 0.1f;
+
+    private bool OnCooldown => timer > 0;
+
+    private float timer;
+
+
+    private void LateUpdate()
+    {
+        if (timer > 0) timer -= Time.deltaTime;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (axis == Axis.Horizontal)
         {
+            if (OnCooldown) return;
+
             if (transform.eulerAngles.y == ROTATION_FACINGLEFT) SendMessageUpwards("HitboxKnockback", Vector2.right);
             else if (transform.eulerAngles.y == ROTATION_FACINGRIGHT) SendMessageUpwards("HitboxKnockback", Vector2.left);
+
+            if (collision.contacts.Length > 0)
+            {
+                Instantiate(hitEffect, collision.contacts[0].point, Random.rotation);
+            }
+
+            timer = COOLDOWN_TIME;
         }
     }
+
+    private enum Axis { Horizontal, Vertical }
 }
