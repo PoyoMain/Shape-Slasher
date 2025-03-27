64dacf55e2a820ac0f9c038deb22d6cfec02b8bb
PoyoMain <CLC9512@GMAIL.COM>
2025-03-26 22:10:43 -0400

    Making prefabs of more map essentials

--- Assets/Scripts/Player/PlayerHitbox.cs
+++ Assets/Scripts/Player/PlayerHitbox.cs
@@ -39,5 +39,16 @@ public class PlayerHitbox : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (axis == Axis.Horizontal)
+        {
+            if (OnCooldown) return;
+
+            if (transform.eulerAngles.y == ROTATION_FACINGLEFT) SendMessageUpwards("HitboxKnockback", Vector2.right);
+            else if (transform.eulerAngles.y == ROTATION_FACINGRIGHT) SendMessageUpwards("HitboxKnockback", Vector2.left);
+        }
+    }
+
     private enum Axis { Horizontal, Vertical }
 }
