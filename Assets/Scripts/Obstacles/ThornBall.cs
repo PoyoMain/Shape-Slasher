7688f96b839d0123a8379379a5f6f7ca59047cac
PoyoMain <CLC9512@GMAIL.COM>
2025-04-16 15:41:14 -0400

    Thorn ball actually destroys when hitting wall

--- Assets/Scripts/Obstacles/ThornBall.cs
+++ Assets/Scripts/Obstacles/ThornBall.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThornBall : MonoBehaviour
 {
     [SerializeField] private CircleCollider2D coll;
+    [SerializeField] private LayerMask wallLayer;
 
     private void Update()
     {
@@ -13,11 +14,6 @@ public class ThornBall : MonoBehaviour
 
     private bool CheckForWall()
     {
-        return Physics2D.Raycast(coll.bounds.center, Vector2.left, coll.radius + 0.01f);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Debug.DrawRay(coll.bounds.center, Vector2.left);
+        return Physics2D.Raycast(coll.bounds.center, Vector2.left, coll.radius + 0.03f, wallLayer);
     }
 }
