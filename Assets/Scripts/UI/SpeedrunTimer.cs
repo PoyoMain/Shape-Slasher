0c01574f1d1e7a53766a6b2b77fea8883a70a65f
PoyoMain <CLC9512@GMAIL.COM>
2025-04-17 20:55:35 -0400

    Speedrun mode in

---
+++ Assets/Scripts/UI/SpeedrunTimer.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SpeedrunTimer : MonoBehaviour
+{
+    [SerializeField] private OptionsSO options;
+    [SerializeField] private TextMeshProUGUI timerTextBox;
+    [SerializeField] private VoidEventSO timerStartEventSO;
+    [SerializeField] private VoidEventSO timerSucceedEventSO;
+    [SerializeField] private VoidEventSO timerFailEventSO;
+
+    public string TimerValue { get; private set; }
+
+    private bool active;
+    private float timer;
+
+    private void Awake()
+    {
+        if (options == null) { Debug.LogError("Options not set in speedrun timer script"); return; }
+        if (timerTextBox == null) { Debug.LogError("Timer textbox not set in speedrun timer script"); return; }
+
+        if (options.SpeedrunMode) timerTextBox.gameObject.SetActive(true);
+        else timerTextBox.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        timerStartEventSO.OnEventRaised += StartTimer;
+        timerSucceedEventSO.OnEventRaised += StopTimer;
+        timerFailEventSO.OnEventRaised += DestoyThis;
+    }
+
+    private void OnDisable()
+    {
+        timerStartEventSO.OnEventRaised -= StartTimer;
+        timerSucceedEventSO.OnEventRaised -= StopTimer;
+        timerFailEventSO.OnEventRaised -= DestoyThis;
+    }
+
+    private void FixedUpdate()
+    {
+        if (active) CountUpTimer();
+    }
+
+    private void CountUpTimer()
+    {
+        timer += Time.fixedDeltaTime;
+        timerTextBox.text = TimeSpan.FromSeconds(timer).ToString("mm\\:ss\\.fff");
+
+    }
+
+    private void StartTimer()
+    {
+        if (options.SpeedrunMode)
+        {
+            timer = 0f;
+            active = true;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void StopTimer()
+    {
+        active = false;
+        TimerValue = timerTextBox.text;
+    }
+
+    public void DestoyThis()
+    {
+        Destroy(gameObject);
+    }
+}
